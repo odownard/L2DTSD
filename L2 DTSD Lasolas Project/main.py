d221d120a08d6ca52398f36c373565cadd094984
odownard <downardo@stu.otc.school.nz>
2025-07-23 14:04:08 +1200

    Layout of website

--- L2 DTSD Lasolas Project/main.py
+++ L2 DTSD Lasolas Project/main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, redirect, request
 import sqlite3
 #render_template is used to render the content from python in order to connect it with HTML.
 
-DATABASE = 'menu.db'
+# This is the database for the application but I can't change the name :)
+DATABASE = 'drinks.db'
 
 #initialises app
 app = Flask(__name__)
@@ -29,12 +30,61 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+# Try commenting out the above parts and running the app to see if it works without them^^^
+
+cart = []
+@app.route("/cart")
+def view_cart():
+    # Display the items in the cart
+    return render_template("cart.html", cart=cart)
+
+# Routes for different pages
 @app.route("/")
 def home():
     #home page
-    sql = "SELECT * FROM pictures"
+    sql = "SELECT * FROM drinks"
     results = query_db(sql)
-    return render_template("index.html",results=results)
+    return render_template("index.html", results=results)
+
+@app.route("/drinks")
+def drinks():
+    # drinks page
+    sql = "SELECT * FROM drinks"
+    results = query_db(sql)
+    return render_template("drinks.html", results=results)
+
+# Below is the code that adds items (drinks or meals) to the cart
+@app.route("/add_to_cart/<string:item_type>/<int:itemID>", methods=["POST"])
+def add_to_cart(item_type, itemID):
+    # Get the item details from the form
+    item = request.form.get("item")
+    price = request.form.get("price")
+    cart.append({
+        "itemID": itemID,
+        "item_type": item_type,
+        "item": item,
+        "price": price
+    })
+    if item_type == "drinks":
+        return redirect("/drinks")
+    elif item_type == "meals":
+        return redirect("/meals")
+    elif item_type == "snacks":
+        return redirect("/snacks")
+    else:
+        return redirect("/")
+
+@app.route("/meals")
+def meals():
+    sql = "SELECT * FROM meals"
+    results = query_db(sql)
+    return render_template("meals.html", results=results)
+
+@app.route("/snacks")
+def snacks():
+    sql = "SELECT * FROM snacks"
+    results = query_db(sql)
+    return render_template("snacks.html", results=results)
     
 if __name__ == "__main__":
     app.run(debug=True)
